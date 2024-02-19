347ee404b9cb9b0125e8d8d9d8820fa565cd6d92
JuHan Park <iiiamst16@gmail.com>
2024-02-19 20:49:51 +0900

    푸터 상위 section 완성

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div className='w-full h-[700px] px-14 pt-24 pb-12'>
-      <div className='flex '></div>
+    <div className="w-full h-[700px] px-14 pt-24 pb-12">
+      <section className="flex relative">
+        <div className="flex-1">
+          <div className="pt-14 relative">
+            <p className="text-3xl font-bold absolute top-0 left-0">
+              오늘의 카카오
+            </p>
+            <p>카카오톡 공식 다운로드</p>
+            <p className="mt-3">#카카오톡백업</p>
+            <p className="mt-3">#카카오 이모티콘 샵</p>
+            <p className="mt-3">#추모프로필</p>
+            <p className="mt-3">#인증서발급</p>
+            <p className="mt-3">#지속가능성</p>
+            <p className="mt-3">#개발자도구</p>
+          </div>
+        </div>
+
+        <div className="flex-1">
+          <div className="pt-24 relative ml-1">
+            <p className="text-lg font-bold absolute top-14">
+              공동체 기업의 서비스
+            </p>
+            <p className="mt-3 text-gray-500">커뮤니케이션</p>
+            <p className="mt-3 text-gray-500">일상편의</p>
+            <p className="mt-3 text-gray-500">비즈니스</p>
+            <p className="mt-3 text-gray-500">쇼핑</p>
+            <p className="mt-3 text-gray-500">엔터테인먼트</p>
+            <p className="mt-3 text-gray-500">임팩트</p>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="pt-24 relative ml-1">
+            <p className="text-lg font-bold absolute top-14">
+              카카오 공동체
+            </p>
+            <p className="mt-3 text-gray-500">카카오</p>
+            <p className="mt-3 text-gray-500">카카오페이</p>
+            <p className="mt-3 text-gray-500">카카오뱅크</p>
+            <p className="mt-3 text-gray-500">카카오모빌리티</p>
+            <p className="mt-3 text-gray-500">카카오스타일</p>
+            <p className="mt-3 text-gray-500">카카오엔터테인먼트</p>
+            <p className="mt-3 text-gray-500">카카오게임즈</p>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="pt-24 relative ml-1">
+            <p className="mt-3 text-gray-500">카카오픽코마</p>
+            <p className="mt-3 text-gray-500">카카오브레인</p>
+            <p className="mt-3 text-gray-500">카카오엔터프라이즈</p>
+            <p className="mt-3 text-gray-500">카카오헬스케어</p>
+            <p className="mt-3 text-gray-500">카카오벤쳐스</p>
+            <p className="mt-3 text-gray-500">카카오인베스트먼트</p>
+            <p className="mt-3 text-gray-500">링키지랩</p>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="pt-24 relative ml-1">
+            <p className="text-lg font-bold absolute top-14">
+              IR
+            </p>
+            <p className="mt-3 text-gray-500">기업지배구조</p>
+            <p className="mt-3 text-gray-500">추가정보</p>
+            <p className="mt-3 text-gray-500">재무정보</p>
+            <p className="mt-3 text-gray-500">IR행사</p>
+            <p className="mt-3 text-gray-500">공시정보</p>
+            <p className="mt-3 text-gray-500">공고</p>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="pt-24 relative ml-1">
+            <p className="text-lg font-bold absolute top-14">
+              계정 및 지원
+            </p>
+            <p className="mt-3 text-gray-500">카카오계정</p>
+            <p className="mt-3 text-gray-500">카카오 프라이버시</p>
+            <p className="mt-3 text-gray-500">카카오 고객센터</p>
+            <p className="mt-3 text-gray-500">다음 고객센터</p>
+            <p className="mt-3 text-gray-500">커머스 고객센터</p>
+            <p className="mt-3 text-gray-500">인재영입</p>
+          </div>
+        </div>
+
+        
+      </section>
     </div>
-  )
+  );
 }
